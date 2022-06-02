560143c392700711c619da5b06afdbb12feecb3c
Christie Renaud <mail@christierenaud.com>
2022-06-02 12:55:46 -0400

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ const User = require('./models/user')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const jwt = require('jsonwebtoken')
+const FacebookTokenStrategy = require('passport-facebook-token')
 
 const config = require('./config.js')
 const { request } = require('./app')
@@ -41,6 +42,37 @@ exports.verifyAdmin = (req, res, next) => {
     return res.send(err.message)
   }
 }
+
+exports.facebookPassport = passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: config.facebook.clientId,
+      clientSecret: config.facebook.clientSecret,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ facebookId: profile.id }, (err, user) => {
+        if (err) {
+          return done(err, false)
+        }
+        if (!err && user) {
+          return done(null, user)
+        } else {
+          user = new User({ username: profile.displayName })
+          user.facebookId = profile.id
+          user.firstname = profile.name.givenName
+          user.lastname = profile.name.familyName
+          user.save((err, user) => {
+            if (err) {
+              return done(err, false)
+            } else {
+              return done(null, user)
+            }
+          })
+        }
+      })
+    },
+  ),
+)
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
